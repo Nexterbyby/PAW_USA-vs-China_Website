7fb9ece6c16dc5d37a7013e6b6552e07866acf48
hpdrucker <hpdrucker@users.noreply.github.com>
2021-12-16 19:55:33 +0100

    Fix css styles

--- southchinasea.js
+++ southchinasea.js
@@ -1,25 +1,13 @@
 window.onload = typeWriter;
 
 var i = 0;
-var j = 0;
-var k = 0;
-var txt = "South";
-var txt2 = "China";
-var txt3 = "Sea";
+var txt = "South China Sea";
 var speed = 80; 
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("heading1").innerHTML += txt.charAt(i);
+    document.getElementById("heading").innerHTML += txt.charAt(i);
     i++;
-  }
-  if (j < txt2.length) {
-    document.getElementById("heading2").innerHTML += txt2.charAt(j);
-    j++;
-  }
-  if (k < txt3.length) {
-    document.getElementById("heading3").innerHTML += txt3.charAt(k);
-    k++;
   }
   setTimeout(typeWriter, speed);
 }
